3a56fdb39e39a357d53a52541453e128571d70d3
bullets012 <lancegenesismercado0111@gmail.com>
2025-07-11 08:47:53 +0800

    first commit

---
+++ src/components/partials/CardStores.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const CardStores = ({
+  image,
+  alt,
+  icon: Icons,
+  title,
+  description,
+  location,
+}) => {
+  return (
+    <div>
+      {/* Store 1 */}
+      <div className="bg-gray-100 rounded-lg overflow-hidden shadow-md hover:shadow-xl transition duration-300">
+        <img
+          image
+          src={image}
+          alt={alt}
+          className="w-full h-48 object-cover"
+        />
+        <div className="p-6">
+          <div className="flex items-center mb-2">
+            <Icons className="text-blue-600 mr-2" />
+            <h3 className="text-xl font-semibold">{title}</h3>
+          </div>
+          <p className="text-gray-600 mb-4">{description}</p>
+          <p className="text-sm text-gray-500">{location}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardStores;
